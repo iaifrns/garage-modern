b55e7aab0178f55fded7d5ae9c991ea6ce663fa6
iaifrns <nsinifranc@gmail.com>
2024-07-10 14:43:24 +0100

    finished the number animation

--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useInView } from 'react-intersection-observer'
 import { useSpring, animated } from 'react-spring'
 
 interface NumberComponentType {
@@ -8,15 +9,17 @@ interface NumberComponentType {
 
 const Numbers: React.FC<NumberComponentType> = ({num, style}) => {
 
+    const [ref, inView] = useInView({triggerOnce: true, threshold: 0.5})
+
     const {number} = useSpring({
         from: {number: 0},
-        number: num,
+        number: inView ? num : 0,
         delay: 200,
         config: {mass: 1, tension: 20, friction: 10}
     })
 
   return (
-    <animated.div className={style}>{number.to((n)=>n.toFixed(0))}</animated.div>
+    <animated.div ref={ref} className={style}>{number.to((n)=>n.toFixed(0))}</animated.div>
   )
 }
 
